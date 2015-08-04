6ae884800047d6a8566271a38edab30f01ec0145
Erwan <erwan@conjecto.com>
2015-08-04 11:43:26 +0200

    fix included frames merge when merging strings

--- src/Conjecto/Nemrod/ElasticSearch/JsonLdFrameLoader.php
+++ src/Conjecto/Nemrod/ElasticSearch/JsonLdFrameLoader.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the Nemrod package.
  *
@@ -8,39 +9,89 @@
  * file that was distributed with this source code.
  */
 
-namespace Conjecto\Nemrod\ElasticSearch;
+namespace Conjecto\Nemrod\Framing\Loader;
 
-use \Conjecto\Nemrod\Framing\Loader\JsonLdFrameLoader as BaseJsonLdFrameLoader;
 use EasyRdf\TypeMapper;
+use Symfony\Component\Templating\TemplateReferenceInterface;
 use Conjecto\Nemrod\ResourceManager\FiliationBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Metadata\MetadataFactory;
 
-class JsonLdFrameLoader extends BaseJsonLdFrameLoader
+/**
+ * FilesystemLoader extends the default Twig filesystem loader
+ * to work with the Symfony paths and jsonld frames references.
+ */
+class JsonLdFrameLoader extends \Twig_Loader_Filesystem
 {
     /**
-     * @var ContainerInterface
+     * @var MetadataFactory
      */
-    protected $container;
+    protected $metadataFactory;
 
     /**
-     * @var string
+     * @param MetadataFactory $metadataFactory
      */
-    protected $esIndex;
-
-    /**
-     * @param ContainerInterface $container
-     */
-    public function setContainer($container)
+    public function setMetadataFactory($metadataFactory)
     {
-        $this->container = $container;
+        $this->metadataFactory = $metadataFactory;
     }
 
     /**
-     * @param $esIndex
+     * @var FiliationBuilder
      */
-    public function setEsIndex($esIndex)
+    protected $filiationBuilder;
+
+    /**
+     * @param FiliationBuilder $filiationBuilder
+     */
+    public function setFiliationBuilder(FiliationBuilder $filiationBuilder)
     {
-        $this->esIndex = $esIndex;
+        $this->filiationBuilder = $filiationBuilder;
+    }
+
+    /**
+     * Return the decoded frame.
+     */
+    public function getFrame($name, $assoc = true)
+    {
+        $decoded = json_decode($this->getSource($name), $assoc);
+        if ($decoded === null) {
+            throw new \Twig_Error_Loader(sprintf('Unable to decode frame "%s".', $name));
+        }
+
+        return $decoded;
+    }
+
+    public function load($name, $type = null, $includeSubFrames = true, $assoc = true, $findTypeInFrames = false)
+    {
+        if ($findTypeInFrames) {
+            $frame = $this->getFrame($name, true);
+            if (isset($frame['@type'])) {
+                $type = $frame['@type'];
+            }
+        }
+
+        if ($includeSubFrames) {
+            $frames = $this->getParentFrames($type);
+            if (!in_array($name, $frames)) {
+                $frames[] = $name;
+            }
+
+            // merge frames together
+            $finalFrame = array();
+            foreach ($frames as $currentFrame) {
+                if (!empty($currentFrame)) {
+                    // find frame with frame path and merge included frames
+                    $frame = $this->mergeWithIncludedFrames($this->getFrame($currentFrame), $assoc);
+                    // merge current frame with other frames
+                    $finalFrame = $this->array_merge_recursive($finalFrame, $frame);
+                }
+            }
+
+            return $finalFrame;
+        }
+        else {
+            return $this->getFrame($name, $assoc);
+        }
     }
 
     /**
@@ -51,15 +102,6 @@ class JsonLdFrameLoader extends BaseJsonLdFrameLoader
      */
     public function getParentFrames($type)
     {
-        $serializerHelper = $this->container->get('nemrod.elastica.serializer_helper');
-        if (!$type) {
-            return array();
-        }
-
-        if (!$this->esIndex) {
-            throw new \Exception('Elasticsearch index is not defined');
-        }
-
         $parentClasses = $this->filiationBuilder->getParentTypes($type);
         $parentFrames = array();
 
@@ -68,9 +110,13 @@ class JsonLdFrameLoader extends BaseJsonLdFrameLoader
         }
 
         foreach ($parentClasses as $parentClass) {
-            $frame = $serializerHelper->getTypeFramePath($this->esIndex, $parentClass);
-            if ($frame) {
-                $parentFrames[] = $frame;
+            $phpClass = TypeMapper::get($parentClass);
+            if ($phpClass) {
+                $metadata = $this->metadataFactory->getMetadataForClass($phpClass);
+                $frame = $metadata->getFrame();
+                if ($frame) {
+                    $parentFrames[] = $metadata->getFrame();
+                }
             }
         }
 
@@ -78,33 +124,153 @@ class JsonLdFrameLoader extends BaseJsonLdFrameLoader
     }
 
     /**
-     * @param $name
-     * @param null $parentClass
-     * @param bool $includeSubFrames
-     * @param bool $assoc
-     * @param bool $getTypeFromFrame
+     * Include subFrames defined we @ include property
+     * @param $frame
+     * @param null $parentKey
+     * @return array
      */
-    public function load($name, $type = null, $includeSubFrames = true, $assoc = true, $findTypeInFrames = false)
+    protected function mergeWithIncludedFrames($frame, $parentKey = null)
     {
-        $frame = parent::load($name, $type, $includeSubFrames, $assoc, $findTypeInFrames);
-        return $this->addMissingProperties($frame);
+        if (!is_array($frame)) {
+            return $frame;
+        }
+
+        $includedFrame = array();
+        foreach ($frame as $key => $subFrame) {
+            if ($key === "@include") {
+                return $this->includeSubFrame($frame, $subFrame);
+            }
+            $includedFrame[$key] = $this->mergeWithIncludedFrames($subFrame, $key);
+        }
+
+        return $includedFrame;
     }
 
-    protected function addMissingProperties($frame)
+    protected function includeSubFrame($frame, $subFrame)
     {
-        $missingProperties = [
-            'rdf:type' => []
-        ];
+        // get frame type before manipulating the frame
+        $initialFrameType = null;
+        if (isset($frame['@type'])) {
+            $initialFrameType = $frame['@type'];
+        }
 
-        foreach ($missingProperties as $key => $value) {
-            if (!isset($frame[$key])) {
-                $frame[$key] = $value;
+        if (is_string($subFrame)) {
+            $includedFrame = $this->getFrame($subFrame);
+            unset($frame["@include"]);
+            $frame = $this->array_merge_recursive($frame, $includedFrame);
+        }
+        else if (is_array($subFrame) && isset($subFrame))
+        {
+            // get parentObjectFrames option before manipulating the frame
+            $parentObjectFrames = null;
+            if (isset($subFrame['parentObjectFrames'])) {
+                $parentObjectFrames = $subFrame['parentObjectFrames'];
             }
-            else {
-                $frame[$key] = array_merge_recursive($value, $frame[$key]);
+
+            // get and merge the included frame
+            unset($frame["@include"]);
+            $includedFrame = $this->getFrame($subFrame);
+            $frame = $this->array_merge_recursive($includedFrame, $frame);
+            // clear the frame
+            unset($frame["@include"]);
+            if ($initialFrameType) {
+                $frame["@type"] = $initialFrameType;
+            }
+
+            // include subFrame parent frames if parentObjectFrames is setted to true
+            if ($parentObjectFrames && isset($frame["@type"])) {
+                $parentClassMetadatas = $this->getParentMetadatas($frame["@type"], array(), true);
+                foreach ($parentClassMetadatas as $classMetadata) {
+                    if ($classMetadata && isset($classMetadata) && !empty($classMetadata)) {
+                        $frame = $this->array_merge_recursive($frame, $this->getFrame($classMetadata));
+                    }
+                }
+                // reset the initial type
+                if (isset($frame['@type'])) {
+                    $types = $frame['@type'];
+                    if (is_array($types)) {
+                        $frame['@type'] = $types[0];
+                    }
+                }
             }
         }
 
-        return $frame;
+        if ($initialFrameType) {
+            $frame["@type"] = $initialFrameType;
+        }
+
+        // recall recursive frame include if included frames have other included frames
+        return $this->mergeWithIncludedFrames($frame);
+    }
+
+    /**
+     * Returns the path to the template file.
+     *
+     * The file locator is used to locate the template when the naming convention
+     * is the symfony one (i.e. the name can be parsed).
+     * Otherwise the template is located using the locator from the twig library.
+     *
+     * @param string|TemplateReferenceInterface $template The template
+     *
+     * @return string The path to the template file
+     *
+     * @throws \Twig_Error_Loader if the template could not be found
+     */
+    protected function findTemplate($template)
+    {
+        $logicalName = (string) $template;
+
+        if (isset($this->cache[$logicalName])) {
+            return $this->cache[$logicalName];
+        }
+
+        $file = null;
+        $previous = null;
+
+        try {
+            $file = parent::findTemplate($logicalName);
+        } catch (\Twig_Error_Loader $e) {
+            throw new \Twig_Error_Loader(sprintf('Unable to find frame "%s".', $logicalName), -1, null, $previous);
+        }
+
+        return $this->cache[$logicalName] = $file;
+    }
+
+    private function array_merge_recursive($array1, $array2)
+    {
+        if (empty($array1) && !empty($array2)) {
+            return $array2;
+        }
+        if (empty($array2) && !empty($array1)) {
+            return $array1;
+        }
+        $finalArray = array();
+        $keys = array_merge(array_keys($array1), array_keys($array2));
+
+        foreach ($keys as $key) {
+            $result = array();
+            if (isset($array1[$key])) {
+                if (isset($array2[$key])) {
+                    if (!is_array($array1[$key])) {
+                        $result = $array1[$key];
+                    }
+                    else if(!is_array($array2[$key])) {
+                        $result = $array1[$key];
+                    }
+                    else {
+                        $result = $this->array_merge_recursive($array1[$key], $array2[$key]);
+                    }
+                }
+                else {
+                    $result = $array1[$key];
+                }
+            }
+            else if (isset($array2[$key])){
+                $result = $array2[$key];
+            }
+            $finalArray[$key] = $result;
+        }
+
+        return $finalArray;
     }
 }
